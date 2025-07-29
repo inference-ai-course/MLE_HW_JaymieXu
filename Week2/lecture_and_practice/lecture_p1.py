b79f2d56bf3192dcf0a6549f27d3fe696ff0e60d
Jaymie Xu <8717103+JaymieX@users.noreply.github.com>
2025-07-29 11:26:00 -0400

    lecture and practice 1-2

---
+++ Week2/lecture_and_practice/lecture_p1.py
@@ -0,0 +1,31 @@
+﻿import numpy as np
+
+# Q K V example, Q K V are already generated dynamically everytime a new sequence is queried
+# and this gives self attention based on context of this qury
+
+"""
+Q = Query — asks “What am I looking for?”
+K = Key — describes “What do I have?”
+V = Value — the information to use if something matches
+"""
+
+# Random Q, K, V matrices
+def generate_random_qkv(seq_len=4, d_model=8):
+    return [np.random.rand(seq_len, d_model) for _ in range(3)]
+
+# Scaled dot-product attention
+def self_attention(Q, K, V):
+    d_k = Q.shape[-1] # size of last dimension of Q aka feature size 8
+    scores = np.dot(Q, K.T) / np.sqrt(d_k) # Q dot K's transposed / scaling step, dot to check how similar vector align
+    weights = softmax(scores) # softmax all add to 1
+    output = np.dot(weights, V) # apply weight to V to get new weighted sum
+    return output, weights
+
+def softmax(x):
+    exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+    return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
+
+Q, K, V = generate_random_qkv()
+out, attn_weights = self_attention(Q, K, V)
+print("Attention Output:\n", out)
+print("Attention Weights:\n", attn_weights)
---
+++ Week2/lecture_and_practice/lecture_p2.py
@@ -0,0 +1,29 @@
+﻿import torch
+import torch.nn as nn
+
+
+class MiniTransformerBlock(nn.Module):
+    def __init__(self, embed_dim):
+        super().__init__()
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads=2, batch_first=True)
+        self.ffn = nn.Sequential(
+            nn.Linear(embed_dim, embed_dim * 4), # expand in this case 16 * 4 = 64
+            nn.ReLU(),
+            nn.Linear(embed_dim * 4, embed_dim) # compress back to 16
+        )
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.norm2 = nn.LayerNorm(embed_dim)
+
+    def forward(self, x):
+        attn_output, _ = self.attn(x, x, x) # self attention Q K V. Since this is self attention it's all x, it will be different on cross attention
+        x = self.norm1(x + attn_output) # Residual then normalize
+        ffn_output = self.ffn(x) # ffn network
+        x = self.norm2(x + ffn_output) # Residual then normalize
+        return x
+
+
+# 1 sentence (batch), 5 words (tokens), each word is a 16-number vector.
+x = torch.randn(1, 5, 16)  # batch_size=1, seq_len=5, embed_dim=16
+model = MiniTransformerBlock(embed_dim=16)
+out = model(x) # py torch nn.Module inheritance __call__ a forward function when used like this
+print(out.shape)
