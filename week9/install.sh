99b22da81f3412e499a5ec353d0f965e68ad2db5
Jaymie Xu <8717103+JaymieX@users.noreply.github.com>
2025-10-02 18:12:55 -0400

    Update install.sh

--- week9/install.sh
+++ week9/install.sh
@@ -4,6 +4,9 @@ sudo apt install ffmpeg
 
 pip install -r requirements.txt
 
+# Override conqui tts dependency nightmare
+pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu124
+
 # Build RAG database
 echo "Building RAG database..."
-python rag/rag_build.py
+python rag2/rag_build.py
